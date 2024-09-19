10e6f0bd85ee578aff316d67ff43724f0a58b136
mathct <mathieu.chatrain@gmail.com>
2024-09-19 17:38:37 +0200

    routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 
 const userRoutes = require('./routes/user');
+const bookRoutes = require('./routes/book');
 
 const app = express();
 
@@ -22,47 +23,7 @@ app.use((req, res, next) => {
   });
 
 
-app.use('/api/books', (req, res, next) => {
-  const books = [
-    {
-        _id: 'oeihfzeoi',
-        userId: 'qsomihvqios',
-        title: 'Mon premier livre',
-        author: 'Mon premier auteur',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        year: 2020,
-        genre: 'Policier',
-        ratings : [
-            {
-            userId : 'qsomihvqios',
-            grade : 2,
-            }
-            ],
-        averageRating : 3,
-    
-      
-    },
-    {
-        _id: 'oeihfzeomoihi',
-        userId: 'qsomihvqios',
-        title: 'Mon second livre',
-        author: 'Mon second auteur',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        year: 2024,
-        genre: 'Historique',
-        ratings : [
-            {
-            userId : 'qsomihvqios',
-            grade : 3,
-            }
-            ],
-        averageRating : 4,
-      
-    },
-  ];
-  res.status(200).json(books);
-});
-
 app.use('/api/auth', userRoutes);
+app.use('/api/books', bookRoutes);
 
 module.exports = app;

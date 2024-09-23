7f6f90eac09f701434e0a4ba6c677109be6d41db
mathct <mathieu.chatrain@gmail.com>
2024-09-23 17:09:08 +0200

    modifier et supprimer

--- controllers/book.js
+++ controllers/book.js
@@ -1,4 +1,5 @@
 const Book = require('../models/book');
+const fs = require('fs');
 
 
 exports.getAllBooks = (req, res, next) => {
@@ -7,7 +8,29 @@ exports.getAllBooks = (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   };
 
+exports.getOneBook = (req, res, next) => {
+Book.findOne({_id: req.params.id})
+    .then((book) => {res.status(200).json(book)})
+    .catch(error => res.status(404).json({ error }));
+};
 
+exports.deleteBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then(book => {
+            if (book.userId != req.auth.userId) {
+                res.status(403).json({ message: 'Unauthorized request' });
+            } else {
+                const filename = book.imageUrl.split('/images/')[1];
+                
+                fs.unlink(`images/${filename}`, () => {
+                    Book.deleteOne({ _id: req.params.id })
+                        .then(() => { res.status(200).json({ message: 'Livre supprimé !' }) })
+                        .catch((error) => {res.status(400).json({error: error})});
+                });
+            }
+        })
+        .catch( error => {res.status(404).json({ error })});
+};
 
 
 exports.createBook = (req, res, next) => {
@@ -26,3 +49,35 @@ book.save()
     .then(() => { res.status(201).json({message: 'Livre enregistré !'})})
     .catch(error => { res.status(400).json( { error })})
 }; 
+
+
+
+exports.modifyBook = (req, res, next) => {
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
+    } : { ...req.body };
+    
+    delete bookObject._userId;
+    
+    Book.findOne({_id: req.params.id})
+        .then((book) => {
+            // updating book only if creator of the book's card
+            if (book.userId != req.auth.userId) {
+                res.status(403).json({ message : 'Unauthorized request' });
+            } else {
+                //  Separation of existing image file name
+                const filename = book.imageUrl.split('/images/')[1];
+                // If the image has been modified, the old one is deleted.
+                req.file && fs.unlink(`images/${filename}`, (err => {
+                        if (err) console.log(err);
+                    })
+                );
+                // updating the book
+                Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Livre mis à jour!' }))
+                    .catch(error => res.status(400).json({ error }));
+            }
+        })
+        .catch(error => res.status(404).json({ error }));
+};
--- routes/book.js
+++ routes/book.js
@@ -8,6 +8,9 @@ const sharp = require('../middleware/sharp-config');
 const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
+router.get('/:id', bookCtrl.getOneBook);
 router.post('/', auth, multer, sharp, bookCtrl.createBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+router.put('/:id', auth, multer, sharp, bookCtrl.modifyBook);
 
 module.exports = router;
